e0ce5fb16ba1467676deba6ea76541eacc450406
LucasEscopelli <67751512+LucasEscopelli@users.noreply.github.com>
2023-05-22 01:50:21 -0300

    trocando botão da documentação por switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,14 @@
 import { Button, HStack, Image, Text, useColorMode } from "@chakra-ui/react";
 import logo from '../assets/logo.webp'
+import ColorModeSwitch from "./ColorModeSwitch";
 
 function NavBar() {
     const { colorMode, toggleColorMode } = useColorMode()
 
   return (
-    <HStack>
+    <HStack justifyContent={'space-between'} padding='10px'>
       <Image src={logo} boxSize='60px'/>
-      <Text>NavBar</Text>
-      <Button onClick={toggleColorMode}>
-        Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-      </Button>
+      <ColorModeSwitch></ColorModeSwitch>
     </HStack>
   );
 }
