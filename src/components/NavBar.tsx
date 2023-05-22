ad91686cc09e59e4208f7d39d4bf1d9201818679
LucasEscopelli <67751512+LucasEscopelli@users.noreply.github.com>
2023-05-22 01:42:33 -0300

    adicionando botão para mudança de tema da página com chakra UI

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,16 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { Button, HStack, Image, Text, useColorMode } from "@chakra-ui/react";
 import logo from '../assets/logo.webp'
 
 function NavBar() {
+    const { colorMode, toggleColorMode } = useColorMode()
+
   return (
     <HStack>
       <Image src={logo} boxSize='60px'/>
       <Text>NavBar</Text>
+      <Button onClick={toggleColorMode}>
+        Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+      </Button>
     </HStack>
   );
 }
