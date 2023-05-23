bf91a3800e72be3dad663aa86a113b71233393e8
LucasEscopelli <67751512+LucasEscopelli@users.noreply.github.com>
2023-05-22 22:36:00 -0300

    adicionando Grid, imagens e reponsividade para os jogos.

--- src/App.tsx
+++ src/App.tsx
@@ -17,11 +17,11 @@ function App() {
        <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="gold">
+        <GridItem area="aside">
           Gold
         </GridItem>
       </Show>
-      <GridItem area="main" bg="dodgerblue">
+      <GridItem area="main" >
         <GameGrid />
       </GridItem>
     </Grid>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,15 +1,20 @@
-import {  Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 function GameGrid() {
-  const {gamesArray, error} = useGames();
+  const { gamesArray, error } = useGames();
 
-  return (<>
-  {error && <Text>{error}</Text>}
-  <ul>
-   { gamesArray.map(game => (<li>{game.name}</li>))}
-  </ul>
-  </>);
+  return (
+    <>
+      {error && <Text>{error}</Text>}
+      <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} padding='10px' spacing={10}>
+        {gamesArray.map((game) => (
+          <GameCard key={game.id} game={game}></GameCard>
+        ))}
+      </SimpleGrid>
+    </>
+  );
 }
 
 export default GameGrid;
