4065310afb9b8bbc16038c36f912b2d99a077e7d
LucasEscopelli <67751512+LucasEscopelli@users.noreply.github.com>
2023-05-23 00:08:32 -0300

    adicionando esqueletos para UX ser melhor quando fetch não tiver sido renderizado.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,14 +1,26 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 function GameGrid() {
-  const { gamesArray, error } = useGames();
-
+  const { gamesArray, error, isLoading } = useGames();
+  const quantidadeSkeletons = 16;
+  const skeletons: number[] = [];
+  for(let i = 0; i < quantidadeSkeletons; i++){
+    skeletons.push(i);
+  }
+  
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} padding='10px' spacing={10}>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding="10px"
+        spacing={10}
+      >
+        {isLoading &&
+          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
         {gamesArray.map((game) => (
           <GameCard key={game.id} game={game}></GameCard>
         ))}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,22 +24,27 @@ interface FetchGamesReponse {
 const useGames = () => {
   const [gamesArray, setGamesArray] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
-
+    setIsLoading(true);
     apiClient
       .get<FetchGamesReponse>("/games", { signal: controller.signal })
-      .then((res) => setGamesArray(res.data.results))
+      .then((res) => {
+        setGamesArray(res.data.results);
+        setIsLoading(false);
+      })
       .catch((error) => {
         if (error instanceof CanceledError) return;
         setError(error.message);
+        setIsLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { gamesArray, error };
+  return { gamesArray, error, isLoading };
 };
 
 export default useGames;
